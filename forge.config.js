c7b98f1c0075c733f1ae04be47ae35d5811e7ea4
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-01-17 16:16:21 +0800

    0.1-beta.11

--- forge.config.js
+++ forge.config.js
@@ -1,22 +1,15 @@
 module.exports = {
-  packagerConfig: {},
-  rebuildConfig: {},
-  makers: [
+  publishers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'zmh-program',
+          name: 'Zh-Website',
+        },
+        prerelease: false,
+        draft: true,
+      },
     },
   ],
-};
+}
