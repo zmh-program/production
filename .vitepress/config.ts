bbe08ed0b7317a40fc46697574bab1db168fa363
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-01-18 16:31:05 +0800

    0.2

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,20 +1,19 @@
-import { defineConfig } from 'vitepress'
-
-module.exports = defineConfig({
+export default {
   title: 'Zh-Website',
   description: 'an Open Source Deep Reinforcement Learning training platform, and we can experience the charm and fun of deep reinforcement learning.' ,
   appearance: 'dark',
   lastUpdated: true,
   markdown: {
     lineNumbers: true,
-    logo: "@/assets/logo.png"
   },
   themeConfig: {
+    logo: "/favicon.ico",
     nav: [
-      {text : "Index", link: "/"},
       {text : "Github", link : "https://github.com/zmh-program/Zh-Website"},
       {text : "Gitee", link : "https://gitee.com/zmh-program/Zh-Website"},
-      {text : "Edit", link : "https://github.com/zmh-program/production"}
+    ],
+    socialLinks: [
+        { icon: 'github', link: 'https://github.com/zmh-program/production'}
     ]
   }
-})
+}
